fa2583b4a2d0bb2bfb0114dce78e4cd234659f94
eddy madu <eddymadu90@gmail.com>
2022-07-08 20:59:15 +0100

    colne complete

---
+++ components/appHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex justify-between py-5 px-3">
+    <!--  -->
+    <div class="flex space-x-3">
+      <div class="mt-5">
+        <i class="fas fa-bars cursor-pointer" @click="$emit('showSideNav')"></i>
+      </div>
+      <div><img src="../static/svg/logo.svg" class="w-16" alt="" /></div>
+      <div
+        class="w-full rounded-sm bg-white px-2 flex space-x-1 border-2 mt-3 rounded-full"
+      >
+        <span class="mt-2">
+          <svg
+            width="16"
+            height="15"
+            viewBox="0 0 16 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.73185 12.8287C8.1551 12.8284 9.53733 12.352 10.6585 11.4753L14.1833 15L15.3171 13.8663L11.7923 10.3416C12.6695 9.22038 13.1462 7.83791 13.1466 6.41437C13.1466 2.87765 10.2688 0 6.73185 0C3.19494 0 0.317139 2.87765 0.317139 6.41437C0.317139 9.95109 3.19494 12.8287 6.73185 12.8287ZM6.73185 1.60359C9.38513 1.60359 11.5429 3.76123 11.5429 6.41437C11.5429 9.06751 9.38513 11.2251 6.73185 11.2251C4.07856 11.2251 1.92082 9.06751 1.92082 6.41437C1.92082 3.76123 4.07856 1.60359 6.73185 1.60359Z"
+              fill="#757575"
+            />
+          </svg>
+        </span>
+        <input
+          type="text"
+          placeholder="Search"
+          class="py-1 w-full font-extrabold border-none outline-none focus:outline-none text-sm"
+        />
+      </div>
+    </div>
+    <div class="flex">
+      <div class="flex space-x-4">
+        <div class="flex space-y-3 space-x-2">
+          <div class="w-full flex mt-3 space-x-1">
+            <p class="font-extrabold text-sm">ENGLISH</p>
+            <i class="fas fa-chevron-down mt-3 fa-xs"></i>
+          </div>
+
+          <div>
+            <i class="fas fa-user"></i>
+          </div>
+        </div>
+
+        <div>
+          <i class="fas fa-cart-arrow-down mt-4"></i>
+        </div>
+      </div>
+      <!--  -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ components/sidenav.vue
@@ -0,0 +1,176 @@
+<template>
+  <div class="py-4 space-y-14 bg-white grey">
+    <!-- logo -->
+    <div class="flex w-full justify-center">
+      <img src="../static/svg/logo.svg" class="w-16" alt="" />
+    </div>
+    <div class="space-y-5 text-sm">
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink to="/" id="router1">
+          <div
+            class="flex justify-between w-full font-extrabold pr-4 pl-9 py-2 rounded-sm"
+          >
+            <p>Router 1</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router2"
+          to="/router2"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 2</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router3"
+          to="/router3"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 3</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router4"
+          to="/"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 4</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router5"
+          to="/router2"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 5</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router6"
+          to="/"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 6</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router7"
+          to="/router1"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 7</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router8"
+          to="/router1"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 8</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router9"
+          to="/router1"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 9</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <div @click="$emit('closeSideNav')">
+        <!--  -->
+        <NuxtLink
+          id="router10"
+          to="/router1"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 10</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router11"
+          to="/router1"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 11</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+      <!--  -->
+      <div @click="$emit('closeSideNav')">
+        <NuxtLink
+          id="router12"
+          to="/router1"
+          class="flex justify-between w-full font-extrabold"
+        >
+          <div class="flex justify-between w-full font-extrabold pr-4 pl-9">
+            <p>Router 12</p>
+            <i class="fas fa-chevron-down fa-xs mt-2"></i>
+          </div>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    makeActive: (id) => {
+      document.getElementById(id).style.backgroundColor = "rgb(218, 218, 252)";
+    },
+  },
+};
+</script>
+
+<style></style>
