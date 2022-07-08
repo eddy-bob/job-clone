821ac8008316b71baabf33c0aaff68959e711e1b
eddy madu <eddymadu90@gmail.com>
2022-07-08 22:09:42 +0100

    colne complete

---
+++ pages/index.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="w-full flex justify-center">
+    <div class="space-y-16">
+      <div class="border-b flex justify-end">
+        <div
+          class="flex space-x-4 font-extrabold justify-start text-sm pb-4 grey"
+        >
+          <!-- main -->
+
+          <div class="w-full flex space-x-20 border-b-2">
+            <p>color</p>
+            <i class="fas fa-chevron-down mt-3 fa-xs"></i>
+          </div>
+
+          <div class="w-full flex space-x-20 border-b-2">
+            <p>material</p>
+            <i class="fas fa-chevron-down mt-3 fa-xs"></i>
+          </div>
+
+          <div class="w-full flex space-x-20 border-b-2">
+            <p>size</p>
+            <i class="fas fa-chevron-down mt-3 fa-xs"></i>
+          </div>
+          <div class="w-full">
+            <button
+              class="text-white py-2 px-3 font-extrabold bg-black rounded-sm text-xs"
+            >
+              APPLY FILTER
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- grid -->
+      <div class="grid sm:grid-cols-4 gap-16 grid-cols-1">
+        <div class="space-y-5 grey">
+          <div class="flex justify-center w-full">
+            <img src="../static/svg/torah.svg" class="w-20" alt="" />
+          </div>
+          <div class="space-y-2">
+            <div class="flex space-x-5 justify-end mr-7">
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: grey"
+              ></p>
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: orange"
+              ></p>
+            </div>
+            <p class="font-extrabold text-xs text-center">SEVEN BRANCH NORAH</p>
+            <p class="font-extrabold text-sm">0.00</p>
+          </div>
+        </div>
+        <!--  -->
+        <div class="space-y-5 grey">
+          <div class="flex justify-center w-full">
+            <img src="../static/svg/torah.svg" class="w-20" alt="" />
+          </div>
+          <div class="space-y-2">
+            <div class="flex space-x-5 justify-end mr-7">
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: grey"
+              ></p>
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: orange"
+              ></p>
+            </div>
+            <p class="font-extrabold text-xs text-center">SEVEN BRANCH NORAH</p>
+            <p class="font-extrabold text-sm">0.00</p>
+          </div>
+        </div>
+        <!--  -->
+        <div class="space-y-5 grey">
+          <div class="flex justify-center w-full">
+            <img src="../static/svg/torah.svg" class="w-20" alt="" />
+          </div>
+          <div class="space-y-2">
+            <div class="flex space-x-5 justify-end mr-7">
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: grey"
+              ></p>
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: orange"
+              ></p>
+            </div>
+            <p class="font-extrabold text-xs text-center">SEVEN BRANCH NORAH</p>
+          </div>
+          <p class="font-extrabold text-sm">0.00</p>
+        </div>
+        <!--  -->
+        <div class="space-y-5 grey">
+          <div class="flex justify-center w-full">
+            <img src="../static/svg/torah.svg" class="w-20" alt="" />
+          </div>
+          <div class="space-y-2">
+            <div class="flex space-x-5 justify-end mr-7">
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: grey"
+              ></p>
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: orange"
+              ></p>
+            </div>
+            <p class="font-extrabold text-xs text-center">SEVEN BRANCH NORAH</p>
+            <p class="font-extrabold text-sm">0.00</p>
+          </div>
+        </div>
+        <!--  -->
+        <div class="space-y-5 grey">
+          <div class="flex justify-center w-full">
+            <img src="../static/svg/torah.svg" class="w-20" alt="" />
+          </div>
+          <div class="space-y-2">
+            <div class="flex space-x-5 justify-end mr-7">
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: grey"
+              ></p>
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: orange"
+              ></p>
+            </div>
+            <p class="font-extrabold text-xs text-center">SEVEN BRANCH NORAH</p>
+            <p class="font-extrabold text-sm">0.00</p>
+          </div>
+        </div>
+        <!--  -->
+        <div class="space-y-5 grey">
+          <div class="flex justify-center w-full">
+            <img src="../static/svg/torah.svg" class="w-20" alt="" />
+          </div>
+          <div class="space-y-2">
+            <div class="flex space-x-5 justify-end mr-7">
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: grey"
+              ></p>
+              <p
+                class="rounded-full h-2 w-2"
+                style="background-color: orange"
+              ></p>
+            </div>
+            <p class="font-extrabold text-xs text-center">SEVEN BRANCH NORAH</p>
+            <p class="font-extrabold text-sm">0.00</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
